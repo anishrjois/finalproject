b8aa66fa39d922b20a2fd48c513f4e743c112cd7
anishrjois <anishrjois@gmail.com>
2025-12-11 12:45:53 -0600

    Create   irreversible_full_adder.v

---
+++ src/irreversible_full_adder.v
@@ -0,0 +1,10 @@
+module irreversible_full_adder(
+  input A,
+  input B,
+  input Cin,
+  output Sum,
+  output Cout
+);
+  assign Sum  = A ^ B ^ Cin;
+  assign Cout = (A & B) | (Cin & (A ^ B));
+endmodule
